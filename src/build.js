ec3e439df9a2714d5a39971b5f910c71a3a8cbf1
Florens Verschelde <florens@fvsch.com>
2020-10-03 20:21:46 +0200

    Fix: docs folder should contain its own assets (GH Pages limitation)

--- src/build.js
+++ src/build.js
@@ -3,34 +3,37 @@ const path = require('path')
 const sass = require('sass')
 const prettier = require('prettier')
 
+const root = path.dirname(__dirname)
+
 const sources = [
-  'remarkdown.scss',
-  'remarkdown.attr.scss',
-  'remarkdown-zero.scss',
-  'remarkdown-zero.attr.scss',
-  'remarkdown-custom.scss',
+  ['src/remarkdown.scss', 'dist/remarkdown.css'],
+  ['src/remarkdown.attr.scss', 'dist/remarkdown.attr.css'],
+  ['src/remarkdown-zero.scss', 'dist/remarkdown-zero.css'],
+  ['src/remarkdown-zero.attr.scss', 'dist/remarkdown-zero.attr.css'],
+  ['src/remarkdown-custom.scss', 'dist/remarkdown-custom.css'],
+  ['docs/demo.scss', 'docs/demo.css'],
 ]
 
 sources.forEach(buildStylesheet)
 
-function buildStylesheet(filename) {
-  const source = path.join(__dirname, filename)
-  if (fs.existsSync(source) === false) {
+function buildStylesheet([sourceFilename, outFilename]) {
+  const sourceFile = path.join(root, sourceFilename)
+  const outFile = path.join(root, outFilename)
+  if (fs.existsSync(sourceFile) === false) {
     return
   }
-  const dest = source.replace('/src/', '/dist/').replace('.scss', '.css')
 
-  console.log('Compiling ' + source)
-  sass.render({ file: source, outputStyle: 'expanded' }, (err, result) => {
+  console.log('Compiling ' + sourceFile)
+  sass.render({ file: sourceFile, outputStyle: 'expanded' }, (err, result) => {
     if (err) throw err
     const css = result.css.toString('utf8')
     const prettified = prettier.format(css, {
       parser: 'css',
       singleQuote: false,
     })
-    fs.writeFile(dest, prettified, (err) => {
+    fs.writeFile(outFile, prettified, (err) => {
       if (err) throw err
-      console.log(`Writing ${dest} (${prettified.length} bytes)`)
+      console.log(`Writing ${outFile} (${prettified.length} bytes)`)
     })
   })
 }
