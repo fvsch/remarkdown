3d2f8953a94f93e9cffd6b2c3b31e54c0b4231c3
Florens Verschelde <243601+fvsch@users.noreply.github.com>
2026-08-04 22:06:10 +0400

    chore: use oxfmt for formatting

--- build.js
+++ build.js
@@ -1,33 +1,40 @@
-import { access, constants, writeFile } from "node:fs/promises"
-import { join } from "node:path"
-import * as prettier from "prettier"
-import * as sass from "sass"
+import { access, constants, writeFile } from 'node:fs/promises';
+import { basename, join } from 'node:path';
+import { compileAsync } from 'sass';
+import { format } from 'oxfmt';
+import oxfmtConfig from './.oxfmtrc.json' with { type: 'json' };
 
-const sources = [
-	["preset/remarkdown.scss", "dist/remarkdown.css"],
-	["preset/remarkdown.attr.scss", "dist/remarkdown.attr.css"],
-	["preset/remarkdown-zero.scss", "dist/remarkdown-zero.css"],
-	["preset/remarkdown-zero.attr.scss", "dist/remarkdown-zero.attr.css"],
-	["docs/demo.scss", "docs/demo.css"],
-]
+buildAll([
+	['preset/remarkdown.scss', 'dist/remarkdown.css'],
+	['preset/remarkdown.attr.scss', 'dist/remarkdown.attr.css'],
+	['preset/remarkdown-zero.scss', 'dist/remarkdown-zero.css'],
+	['preset/remarkdown-zero.attr.scss', 'dist/remarkdown-zero.attr.css'],
+	['docs/demo.scss', 'docs/demo.css'],
+]);
 
-sources.forEach(buildStylesheet)
+async function buildAll(sources) {
+	const results = await Promise.all(sources.map(([input, out]) => buildStylesheet(input, out)));
+	console.table(results.filter(Boolean), ['input', 'out', 'size']);
+}
 
-async function buildStylesheet([sourceFilename, outFilename]) {
-	const sourceFile = join(import.meta.dirname, sourceFilename)
-	const outFile = join(import.meta.dirname, outFilename)
+async function buildStylesheet(inputFilename, outFilename) {
+	const inputFile = join(import.meta.dirname, inputFilename);
+	const outFile = join(import.meta.dirname, outFilename);
 
 	try {
-		await access(sourceFile, constants.R_OK)
+		await access(inputFile, constants.R_OK);
 	} catch {
-		console.error(`Missing file ${sourceFile}`)
-		return
+		console.error(`Missing file ${inputFile}`);
+		return { input: inputFilename, out: null, size: null };
 	}
 
-	const compiled = await sass.compileAsync(sourceFile)
-	const prettified = await prettier.format(compiled.css, {
-		parser: "css",
-	})
-	await writeFile(outFile, prettified)
-	console.log(`${sourceFilename} -> ${outFilename} (${prettified.length} B)`)
+	const { css } = await compileAsync(inputFile);
+	const { code } = await format(basename(outFile), css, oxfmtConfig);
+	await writeFile(outFile, code);
+
+	return {
+		input: inputFilename,
+		out: outFilename,
+		size: code.length + ' B',
+	};
 }
